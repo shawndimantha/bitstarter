2d07bf33013ae4617357d7600ad1ec5f12c4a888
shawndimantha <shawn.dimantha@gmail.com>
2013-07-08 21:11:42 +0000

    Added reading of file index.html

--- web.js
+++ web.js
@@ -2,9 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var fileconts = fs.readFileSync('index.html','utf-8');
+
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-    response.send(fs.readFileSync('index.html','utf-8'));
+//    response.send('Hello World 2!');
+      response.send(fileconts);
 });
 
 var port = process.env.PORT || 5000;
